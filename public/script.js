3fdab06dd12271f07990a37953c7b214f199002a
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2023-08-31 19:54:32 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -11,81 +11,76 @@ function setItems() {
             link: "https://valeriodellefave.github.io/budget-manager",
             isDone: true
         },
-        {
-            title: "endpoints", 
-            link: "https://valeriodellefave.github.io/endpoints",
-            isDone: false
-        },
-        {
-            title: "ValeriodelleFave.github.io", 
-            link: "https://valeriodellefave.github.io/",
-            isDone: true
-        },
-        {
-            title: "budget-manager", 
-            link: "https://valeriodellefave.github.io/budget-manager",
-            isDone: true
-        },
+        // {
+        //     title: "endpoints", 
+        //     link: "https://valeriodellefave.github.io/endpoints",
+        //     isDone: false
+        // },
+        // {
+        //     title: "ValeriodelleFave.github.io", 
+        //     link: "https://valeriodellefave.github.io/",
+        //     isDone: true
+        // },
         {
             title: "Weather", 
             link: "https://valeriodellefave.github.io/weather",
             isDone: true
         },
-        {
-            title: "form-generator", 
-            link: "https://github.com/ValeriodelleFave/form-generator",
-            isDone: false
-        },
-        {
-            title: "js-test", 
-            link: "https://github.com/ValeriodelleFave/js-test",
-            isDone: false
-        },
-        {
-            title: "human-muscles", 
-            link: "https://github.com/ValeriodelleFave/human-muscles",
-            isDone: false
-        },
-        {
-            title: "AngularDevTools", 
-            link: "https://github.com/ValeriodelleFave/AngularDevTools",
-            isDone: false
-        },
-        {
-            title: "db-manager", 
-            link: "https://github.com/ValeriodelleFave/db-manager",
-            isDone: false
-        },
-        {
-            title: "my-store", 
-            link: "https://github.com/ValeriodelleFave/my-store",
-            isDone: false
-        },
-        {
-            title: "istudy", 
-            link: "https://github.com/ValeriodelleFave/istudy",
-            isDone: false
-        },
-        {
-            title: "guide", 
-            link: "https://github.com/ValeriodelleFave/guide",
-            isDone: false
-        },
-        {
-            title: "app-center", 
-            link: "https://github.com/ValeriodelleFave/app-center",
-            isDone: false
-        },
+        // {
+        //     title: "form-generator", 
+        //     link: "https://github.com/ValeriodelleFave/form-generator",
+        //     isDone: false
+        // },
+        // {
+        //     title: "js-test", 
+        //     link: "https://github.com/ValeriodelleFave/js-test",
+        //     isDone: false
+        // },
+        // {
+        //     title: "human-muscles", 
+        //     link: "https://github.com/ValeriodelleFave/human-muscles",
+        //     isDone: false
+        // },
+        // {
+        //     title: "AngularDevTools", 
+        //     link: "https://github.com/ValeriodelleFave/AngularDevTools",
+        //     isDone: false
+        // },
+        // {
+        //     title: "db-manager", 
+        //     link: "https://github.com/ValeriodelleFave/db-manager",
+        //     isDone: false
+        // },
+        // {
+        //     title: "my-store", 
+        //     link: "https://github.com/ValeriodelleFave/my-store",
+        //     isDone: false
+        // },
+        // {
+        //     title: "istudy", 
+        //     link: "https://github.com/ValeriodelleFave/istudy",
+        //     isDone: false
+        // },
+        // {
+        //     title: "guide", 
+        //     link: "https://github.com/ValeriodelleFave/guide",
+        //     isDone: false
+        // },
+        // {
+        //     title: "app-center", 
+        //     link: "https://github.com/ValeriodelleFave/app-center",
+        //     isDone: false
+        // },
         {
             title: "FitCalculator", 
             link: "https://github.com/ValeriodelleFave/FitCalculator",
-            isDone: false
+            isDone: true
         },
-        {
-            title: "Real_Life", 
-            link: "https://github.com/ValeriodelleFave/Real_Life",
-            isDone: false
-        },
+        // {
+        //     title: "Real_Life", 
+        //     link: "https://github.com/ValeriodelleFave/Real_Life",
+        //     isDone: false
+        // },
     ];
 
     for (const app of apps) {
